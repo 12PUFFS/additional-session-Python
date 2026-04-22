2cb1c8b34322cfa19592f3871630921cb38c3657
gal tru <evgenypech98@gmail.com>
2026-04-22 22:37:57 +0300

    module 1

---
+++ manager/products/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Material, MaterialType, MaterialsK, Materialsupplier, Provider, Unit
+
+admin.site.register(MaterialsK)
+admin.site.register(Materialsupplier)
+# admin.site.register(Material)
+# admin.site.register(MaterialType)
+# admin.site.register(Provider)
+# admin.site.register(Unit)
+
+
+admin.site.unregister(Group)
+
+# Register your models here.
