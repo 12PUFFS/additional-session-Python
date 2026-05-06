9e1184ce20074f6620620a5aaf511a7a3fa9a15c
gal tru <evgenypech98@gmail.com>
2026-05-06 21:53:10 +0300

    3

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -126,7 +126,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [BASE_DIR, 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGOUT_REDIRECT_URL = 'login'
